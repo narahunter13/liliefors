df0715daac9c2082e4c6db8ab073b45579231c42
narahunter13 <narahunter13@gmail.com>
2021-07-01 11:30:48 +0700

    Navbar

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,6 +6,10 @@ class Home extends BaseController
 {
 	public function index()
 	{
-		return view('welcome_message');
+		$title = "Liliefors 2021";
+		$data = [
+			'title' => $title
+		];
+		return view('_layout', $data);
 	}
 }
