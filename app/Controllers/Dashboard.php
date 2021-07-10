bde058ddd418aeca6cbc6c76fe21e1fb7b639fcc
narahunter13 <narahunter13@gmail.com>
2021-07-10 13:38:50 +0700

    Dashboard Peserta Fix Judul

--- app/Controllers/Dashboard.php
+++ app/Controllers/Dashboard.php
@@ -9,6 +9,28 @@ class Dashboard extends BaseController
 	public function index()
 	{
 		//
-		return view('dashboard');
+		$data['title'] = 'Tentang Kami';
+		return view('dashboard/index', $data);
+	}
+
+	public function pengumuman()
+	{
+		//
+		$data['title'] = 'Pengumuman';
+		return view('dashboard/pengumuman', $data);
+	}
+
+	public function linimasa()
+	{
+		//
+		$data['title'] = 'Linimasa';
+		return view('dashboard/linimasa', $data);
+	}
+
+	public function sunting()
+	{
+		//
+		$data['title'] = 'Sunting Profil';
+		return view('dashboard/sunting', $data);
 	}
 }
