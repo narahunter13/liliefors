6f7faab9f7ac81a0587bc3b9ef549fbccc29ce9b
narahunter13 <narahunter13@gmail.com>
2021-08-25 09:41:22 +0700

    Fix Upload Karya

--- app/Controllers/User/Unggah_Desain.php
+++ app/Controllers/User/Unggah_Desain.php
@@ -5,13 +5,14 @@ namespace App\Controllers\User;
 use App\Controllers\BaseController;
 use App\Models\ModelPeserta;
 
-class Unggah_Desain extends BaseController {
+class Unggah_Desain extends BaseController
+{
 
     private $session;
     private $active = "desain";
     private $m_peserta;
     private $EXT_FILE = "zip,rar,pdf";
-    private $PATH_FILE = "4-%^&";
+    private $PATH_FILE = "desain";
     private $PREFIX = "Desain_";
 
     public function __construct()
@@ -25,32 +26,26 @@ class Unggah_Desain extends BaseController {
         $data['status'] = $this->m_peserta->get_status_pengumpulan($this->session->get('id'), 4);
         $data['active'] = $this->active;
         $data['title'] = 'Unggah Karya Desain';
-		return view('user/' . $this->active . '/index', $data);
+        return view('user/' . $this->active . '/index', $data);
     }
 
     public function send()
     {
-        $validate = true;
+        // $validate = true;
         // $validate = $this->validate([
         //     'karya' => 'uploaded[karya]|ext_in[karya,' . $this->EXT_FILE . ']'
         // ]);
+        $file = $this->request->getFile('karya');
+        $id = $this->session->get('id');
 
-        if($validate) {
-            $file = $this->request->getFile('karya');
-            $id = $this->session->get('id');
+        $nama_file = $this->PREFIX . $this->session->get('nama') . "." . $file->getExtension();
+        $file->move(WRITEPATH . "karya/" . $this->PATH_FILE, $nama_file, true);
 
-            $nama_file = $this->PREFIX . $this->session->get('nama') . "." . $file->getExtension();
-            $file->move(WRITEPATH . "lie%^&L^^/" . $this->PATH_FILE, $nama_file, true);
-
-            if($this->m_peserta->ubah_status_pengumpulan($id, 4)) {
-                $this->m_peserta->ubah_status_pengumpulan($id, 4);
-                return redirect()->to(base_url('user/unggah-' . $this->active));
-            } else {
-                return redirect()->to(base_url('user/unggah-' . $this->active))->with('error', 'Kesalahan mengirim file!');
-            }
+        if ($this->m_peserta->ubah_status_pengumpulan($id, 4)) {
+            $this->m_peserta->ubah_status_pengumpulan($id, 4);
+            return redirect()->to(base_url('user/unggah-' . $this->active));
         } else {
-            return redirect()->to(base_url('user/unggah-' . $this->active))->with('error', 'Pastikan format file adalah ' . $this->EXT_FILE);
+            return redirect()->to(base_url('user/unggah-' . $this->active))->with('error', 'Kesalahan mengirim file!');
         }
     }
-
 }
